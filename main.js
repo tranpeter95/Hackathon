deef0573474926fdad59b28175517fbc73792a74
Khaile Tran <khailetran94@gmail.com>
2023-05-04 01:06:09 -0400

    fourth commit rough final draft

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 // window.addEventListener("DOMContentLoaded", () => {
 console.log("hello")
 // links to redirect user to after timer runs out
-const links =  ['http://www.w3schools.com', ]
+const links =  ['http://www.w3schools.com' ]
 
-//function to prompt user for timer input 
+//code to check if user has returned to the site
 
 if (localStorage.getItem('hasRedirected') === 'true') {
     alert('You have already visited this page, you will be locked out for 1 hour!');
@@ -18,23 +18,38 @@ if (localStorage.getItem('hasRedirected') === 'true') {
         div.parentNode.removeChild(div);
     })
     document.body.appendChild(will);
+    let timeOut = 60000;
+    const timeOutTimer = document.createElement('div');
+    timeOutTimer.setAttribute('id', 'timeOutTimer');
+    timeOutTimer.style.fontSize = '30px';
+    timeOutTimer.style.color = 'red';
+    timeOutTimer.innerText = timeOut.toString();
+    document.body.appendChild(timeOutTimer);
+    function timeOutCountDown (timeOut) {
+    if (timeOut > 0) {
+        setTimeout(() => {
+            timeOutTimer.innerText = timeOut.toString();
+            timeOutCountDown(timeOut - 1);
+        }, 1000);
+    } else {
+        localStorage.setItem('hasRedirected', 'false');
+    alert('You have served your time! Refresh');
+    }
+}
+timeOutCountDown(timeOut);
 } else {
 
 const aNumber = Number(window.prompt("How long should you be on this website?", ""));
 
 
-// when you load onto the site, it asks for time you should spend on it,
-// when time runs out, you should be locked out of it for x time
-// in order to do this, could have a property set on the website
-// {canVist : true <--- starting}
-// after they get redirected from the site, {canVisit : false};
-// when they visit site, if canVisit is false, they see something else (maybe  a timer that shows how long before they can visit again) 
-
-//add timer to when you return to the page
 //change the url 
 //add more css styling and pictures
+//refactor code
 
-
+// realized issues
+// timer will prob reset anytime u click on new link in page
+// need to change key in localstorage to be specifically the website,
+// since the lockout should be active for some sites but not others
 
 
 
@@ -50,8 +65,8 @@ timer.style.color = 'red';
 let timeSetByUser = aNumber;
 timer.innerText = timeSetByUser.toString();
 document.body.appendChild(timer);
-// function that redirects the user
-// // function that updates the timer element every second
+
+//function to count down the timer
 function countDown(timeSetByUser) {
     if (timeSetByUser > 0) {
         setTimeout(() => {
@@ -60,6 +75,7 @@ function countDown(timeSetByUser) {
             countDown(timeSetByUser - 1);
         }, 1000);
     } else {
+        //assigning prop to localStorage to check if user has redirected to website
         localStorage.setItem('hasRedirected', 'true');
         window.location.replace(links[0])
     }
